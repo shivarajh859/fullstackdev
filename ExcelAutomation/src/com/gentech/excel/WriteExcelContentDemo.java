96ebdcb226d29ae2d4d0acf0d2398b6e566714a0
shivu <shivaraj1234555@gmail.com>
2024-08-31 17:37:06 +0530

    Excel Demo

---
+++ ExcelAutomation/src/com/gentech/excel/WriteExcelContentDemo.java
@@ -0,0 +1,60 @@
+package com.gentech.excel;
+
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcelContentDemo {
+	public static void main(String[] args) {
+		writeContent();
+
+	}
+	
+	private static void writeContent()
+	{
+		FileOutputStream fout=null;
+		Workbook wb=null;
+		Sheet sh=null;
+		Row row=null;
+		Cell cell=null;
+		try
+		{
+			wb=new XSSFWorkbook();
+			sh=wb.createSheet("Information");
+			//1st Row of Data
+			row=sh.createRow(0);
+			cell=row.createCell(0);
+			cell.setCellValue("UserName");
+			cell=row.createCell(1);
+			cell.setCellValue("Password");
+			//2nd row of Data
+			row=sh.createRow(1);
+			cell=row.createCell(0);
+			cell.setCellValue("admin");
+			cell=row.createCell(1);
+			cell.setCellValue("Welcome12345");
+			
+			fout=new FileOutputStream("D:\\EXCEL\\Credentails.xlsx");
+			wb.write(fout);
+		}catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				fout.close();
+				wb.close();
+			}catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
